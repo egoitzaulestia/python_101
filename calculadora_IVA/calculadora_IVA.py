cb6cc6adaf95133e297ccc8c64cbad21cc7df385
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-02-15 14:03:09 +0100

    Update calculadora_IVA.py

--- calculadora_IVA/calculadora_IVA.py
+++ calculadora_IVA/calculadora_IVA.py
@@ -1,40 +1,51 @@
-#TIPOS DE IVA
-#—————
-#IVA tipo general       = 21%
-#IVA tipo reducido      = 10%
-#IVA tipo superreducido = 4%
+#Prototipo de la aplicación "Calculadora IVA" 
+#realizado para el Banco Santander.
 
-#_00_
-#¿CÓMO CALCULAR EL IVA?
-#Pata calcular el IVA que vamos a repercutir o soportar en un factura, 
-#hay que terner tres aspectos claros:
-#   • La BASE IMPONIBLE que corresponde a cada concepto
-#   • El TIPO DE GRAVAMEN que debe aplicarse a cada concepto
-#   • La FÓRMULA del IVA
-
-#_00.1_
-#FÓRMULA PARA CALCULAR EL IVA
-#
-#   IVA=i=1nBI¡*Tg¡
-#
-#   • "IVA" es el importe total del IVA de una factura.
-#   • "BI¡" es la base imponible del concepto número "i" de la factura
-#   • "TG¡" es el tipo de gravamen que se aplica al concepto "i" de la factura.
-#   • Hay "n" conceptos, cada uno con su base imponible y tipo de gravamen
+#El prototipo debe de falitar al usuario:
+#   1 _ Calcular la suma del IVA a un precio que no lleva IVA.
+#     + Para ello facilitar al usuario: 
+#       - Introduccir precio sin IVA
+#       - Intrudición del porcentaje de IVA
+#   2 _ Calcular la resta del iva a un precio que incluye iva.
+#     + Para ello facilitar al usuario:  
+#       - Introducir precio toral con IVA
+#       - Introducir porcentaje del IVA que lleva el producto
 
 
 #Código del programa de la Calculadora IVA
-print("Calculadora IVA") #Imprimimos título
-precio_sin_iva = float(input("(intoduce) Precio sin IVA: "))
-#print(f"{amount} €")
-iva_percentage = float(input("(introduce) Porcenta del IVA: "))
-#print(f"{tax} %")
-iva_total = precio_sin_iva * (iva_percentage / 100)
-precio_total = precio_sin_iva + iva_total
 print()
-print(f"Precio sin IVA: {precio_sin_iva:.2f} €")
-print(f"IVA: {iva_total:.2f} €")
-print(f"Precio con IVA: {precio_total:.2f} €")
-#iva_total = amount * (iva_percentage / 100)
-
-
+print("///// ""\033[1m" + "Banco Santander | Impulsa Empresa"  + "\033[0m" " \\\\\\\\\\")
+print("////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+print("///// ""\033[1m" + "         Calculadora IVA         " + "\033[0m" " \\\\\\\\\\")
+print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ //////////////////////")
+print()
+print("Bienvenido a la calculafora IVA del Santander | Impulsa Empresa.")
+print()
+choice = input("Si quiere sumar el IVA a un precio escriba ""\033[1m" + "\"sumar\"" + "\033[0m" + "\nSi quiere restar el IVA a un precio escriba ""\033[1m" + " \"restar\"" + "\033[0m" + "\n")
+print()
+if choice == "sumar" or choice == "Sumar" or choice == "SUMAR":
+    precio_sin_iva = float(input("///// (introduce) Precio sin IVA: "))
+    iva_percentage = float(input("///// (introduce) Porcenta de IVA: "))
+    iva_total = precio_sin_iva * (iva_percentage / 100)
+    precio_total = precio_sin_iva + iva_total
+    print()
+    print(f"Porcentage de IVA seleccionado: {iva_percentage} %")
+    print(f"Precio sin IVA: {precio_sin_iva:.2f} €")
+    print(f"IVA: {iva_total:.2f} €")
+    print(f"Precio con IVA: {precio_total:.2f} €")
+elif choice == "restar" or choice == "Restar" or choice == "RESTAR":
+    precio_total = float(input("///// (introduce) Precio con IVA: "))
+    iva_percentage = float(input("///// (introduce) Porcenta de IVA: "))
+    superporcentaje_iva = (iva_percentage + 100) 
+    iva_total = (precio_total * iva_percentage) / superporcentaje_iva
+    precio_sin_iva = precio_total - iva_total
+    print()
+    print(f"Porcentage de IVA seleccionado: {iva_percentage} %")
+    print(f"Precio sin IVA: {precio_sin_iva:.2f} €")
+    print(f"IVA: {iva_total:.2f} €")
+    print(f"Precio con IVA: {precio_total:.2f} €")
+else:
+    print("\033[1m" + "¡ERROR!" + "\033[0m")
+    print("Lo sentimos, no ha introducido las palabras correctas.")
+    print("Reinicie el programa, por favor.")
+print()
