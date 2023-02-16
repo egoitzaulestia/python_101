5889323a659ce24663b7e20b87bb5c9126540c41
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-02-16 15:54:28 +0000

    create test 4

---
+++ test4.py
@@ -0,0 +1,59 @@
+### BIBLIOTECAS ###
+#Módulos, librerías, bibliotecas, ...
+
+#help("modules") #Nos da info 
+#help("this")
+
+#___Teoría_de_Clase
+#import this #Importar una librería
+#import os   #Importar una librería
+
+#from this import d,s #Para importar un módulo (o dos) de una librería
+
+#import this as t #Para crear un alias 
+
+#print(d)
+
+
+#___Ejercicios_de_Clase
+
+#Opción 1
+# import this
+
+# #print(this.c)
+# #print(this.s)
+# print(this.i)
+# #print(this.d)
+
+# #Opción 2
+# import this as t
+# print(t.c)
+# print(t.s)
+
+# #Opción 3
+# from this import c, s
+
+# print(c)
+# print(s)
+
+
+#Bibliotecas útiles y divertidas
+
+from urllib.request import urlopen
+
+page = urlopen("http://info.cern.ch/")
+content = page.read()
+
+print(content)
+
+import requests #Uses urllib3
+
+currency = "eur"
+basecurrency = "aud"
+
+url = "http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currency + "&f=" + basecurrency + "&v=1&s=cbr"
+
+resp = request.get(url)
+
+print(resp.text)
+print(resp.json())
