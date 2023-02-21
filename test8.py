1ca565aa1678e2c702afe1c630f601f796149d9b
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-02-21 17:55:28 +0100

    Create test8.py

---
+++ test8.py
@@ -0,0 +1,17 @@
+# i = 1
+# ch = "*"
+# while i < 10:
+#     print(ch * i)
+#     i = i + 2
+# else:
+#     print("----------")
+
+# i = 9
+# while i > 0:
+#   print(ch*i)
+#   i = i - 2
+
+# while True:
+#     pwd1 = input("Introducir tu ci")
+
+
