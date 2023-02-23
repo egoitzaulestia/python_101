a398c9bf41810f43dc9a1ec515d6e852d72c3c8e
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-02-23 16:11:06 +0100

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,46 @@
+#FUNCIONES
+
+def imprimirMensaje():
+    print("Python es un lenguaje de progamción.")
+    print("Me gusta programar con Python")
+
+def imprimirMensaje2(lenguaje): # Le metemos un parametro
+    print(f"{lenguaje} es un lenguaje de progamción.")
+
+def calculo(a): # Le metemos un parametro
+    return a + a + a
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+# Este es el punto de entrada
+if __name__ == '__main__':
+    print("Python es un lenguaje de progamción.")
+    print("Me gusta programar con Python")
+
+    imprimirMensaje()
+    imprimirMensaje()
+
+    x = imprimirMensaje2("Python")
+    #imprimirMensaje2("Java")
+    print(x)
+    y = calculo(5)
+    print(y)
+
+    s = sumar(5, 10)
+    r = restar(5, 10)
+    print(s)
+    print(r)
+
+    firstNum = int(input("Introduce el primer número: "))
+    secondNum = int(input("Introducir el segundo número: "))
+    # suma = sum(firstNum, secondNum)
+    print(sumar(firstNum, secondNum))
+
+    firstNum = int(input("Introduce el primer número: "))
+    secondNum = int(input("Introduce el segundo número: "))
+    print(restar(firstNum, secondNum))
+
