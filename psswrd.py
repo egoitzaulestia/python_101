d6358d2ee15c54e8e1118e3fd0c5a6e6d33ea1f7
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-02-23 17:52:00 +0100

    Create psswrd.py

---
+++ psswrd.py
@@ -0,0 +1,16 @@
+# imprimir un mensaje para describit el programa
+# pregurntar al usuario para un texto para cifra
+texto = 
+list = []
+
+# usar lista.append() para agregar los números a la lista.
+# para hacerlo más complicado todavía, añadir 10 al número, 
+# así que si alguien intenta descifrarlo con chr(), no va funcionar
+for i in texto:
+
+# mostrar la lista de la cadena cifrado - mandarlo a tu compañero
+
+
+# ahora descifra la lista - los números unicode de cada carácter esta en la lista
+for j in range(0, ______):
+    print(_____(lista[____]), end = " ")
