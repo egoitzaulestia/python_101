2b41eebcca01c8f640dd5ce57e53481d9337b46e
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-01 15:20:27 +0100

    Create calculadora_arq.py

---
+++ arquitectura/common/calculadora_arq.py
@@ -0,0 +1,10 @@
+def squareMeter():
+    anchura = float(input("Introduce los metros de anchura: "))
+    longitud = float(input("Introduce los mtros de longitud: "))
+    m2 = float(anchura * longitud)
+    return m2
+
+
+# main creado para depurar
+# if __name__ == "__main__":
+#     print(squareMeter(3, 8))
