ac92d57656960ebf21cd0351aa73f5b3b40e63ce
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-01 15:13:44 +0100

    Create f_message.py

---
+++ mensajes/teimpo/f_message.py
@@ -0,0 +1,12 @@
+import time
+
+DORMIR = 5
+
+def msg():
+    print("Me he despetado.")
+
+def msg_medio():
+    print("Estoy despierto. Estoy, despierto.")
+
+def msg_final():
+    print("LISTO! Vamos para arriba. ESTOY DESPIERTO.")
