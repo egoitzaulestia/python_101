d67588c453c4e7d003f4d3b8d6b04cc573bc878c
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-01 15:12:43 +0100

    Create main.py

---
+++ mensajes/main.py
@@ -0,0 +1,10 @@
+from tiempo.f_message import *
+
+# Programa principal
+if __name__ == "__main__":
+    time.sleep(DORMIR)
+    msg()
+    time.sleep(DORMIR)
+    msg_medio()
+    time.sleep(DORMIR)
+    msg_final()
