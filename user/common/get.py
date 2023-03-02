3e575f39dce43c4c679306e86e5262f6fa81f1e8
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-02 16:37:35 +0100

    Create get.py

---
+++ user/common/get.py
@@ -0,0 +1,11 @@
+def get_usuario():
+    usuario = input("Introdudir tu nombre de usuario: ")
+    return usuario
+
+def get_edad():
+    edad = input("Introduce tu edad: ")
+    return edad
+
+def get_password():
+    password = input("Introduce tu password: ")
+    return password
