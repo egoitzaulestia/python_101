069b5fad829a5a76733960242d21e4f8a81b373c
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-02 16:42:52 +0100

    Create num.py

---
+++ numbers/common/num.py
@@ -0,0 +1,8 @@
+def get_numero():
+    numero = int(input("Introduce tu numero: "))
+    return numero
+
+def repetir_numero(numero):
+    for i in range(numero):
+        print(int(numero))
+        
