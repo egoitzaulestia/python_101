94c65acc98eb689a6c5d85e5a32a7d7b63646e18
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-02 16:46:08 +0100

    update calculo.py

--- modulo_avanzado/common/calculo.py
+++ modulo_avanzado/common/calculo.py
@@ -1,7 +1,13 @@
-from common.calculo import *
+# función para sumar dos valores
+def sumar(a, b):
+    return a + b
+
+# Función para restar dos
+def restar(a, b):
+    return a - b
 
 
+# Dejamos este name/main para depurar
 if __name__ == "__main__":
     print(sumar(2, 5))
     print(restar(3, 5))
-    print(sumar(20, 4))
