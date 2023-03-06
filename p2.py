a09dc2adde96d715c27facd2558d6db6c80147a5
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-06 22:27:39 +0100

    Update p2.py

--- p2.py
+++ p2.py
@@ -15,26 +15,45 @@ class Perro:
         print(f"{self.nombre} dice: \"GUAU!-GUAU!\"")
 
     def presentar(self):
-        print(f"{self.nombre} mide {self.altura} y su raza es {self.raza}")
+        print(f"{self.nombre}, es un perro de la raza {self.raza} y mide {self.altura} metros.")
 
 # Este es el programa principal
 if __name__ == "__main__":
 
+    # miles = Perro("Miles", "Jack Russell Terrier", 0.55)
+    # buddy = Perro("Buddy", "Dachshund", 0.7)
+    # ozzy  = Perro("Ozzy", "French Bulldog", 0.4)
+    # dax   = Perro("Dax", "Spanish Mastiff", 1.35)
+
+    # perros = [miles, buddy, ozzy, dax]
+
+    # for perro in perros:
+    #     # print(f"{perro.nombre} es un perro de la raza {perro.raza} y mide {perro.altura}")
+    #     perro.presentar()
+
+    # print(f"{perros[0].nombre} es un perro de la raza {perros[0].raza} y mide {perros[0].altura}")
+
     perros = []
-    
-    # archivado = input("¿Quieres crear la ficha de un perro nuevo? (Y/N)")
-    
-    # while archivado != "n":
-    #     nombre = input("Nombre de el perro: ")
-    #     raza = input("Raza del perro: ")
-    #     altura = input("Altura del perro: ")
-    
-    #     perro = Perro(nombre, raza, altura)
+    contador = 0
 
-    #     perros.append(perro)
+    archivado = input("¿Quieres crear la ficha de un perro nuevo? (Y/N)")
+    
+    while archivado.upper() == "Y":
 
-    miles = Perro("Miles", "Jack Russell Terrier", 1.2)
-    buddy = Perro("Buddy", "Dachshund", 0.3)
+        nombre = input("Nombre del perro: ")
+        raza = input("Raza del perro: ")
+        altura = input("Altura del perro: ")
+    
+        perro = Perro(nombre, raza, altura)
 
+        perros.append(perro)
+
+        archivado = input("¿Quieres crear la ficha de un perro nuevo? (Y/N)")
+        if archivado.upper() == "Y":
+            continue
+        else:
+            break
+    contador += 1
+    
     for perro in perros:
-        print(perro.nombre)
+        perro.presentar()
