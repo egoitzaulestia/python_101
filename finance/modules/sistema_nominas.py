d1f468a4939ea03c36fb20a7d6b3b61d50c0ba86
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-09 17:51:43 +0100

    Update sitema_nominas.py

--- finance/modules/sistema_nominas.py
+++ finance/modules/sistema_nominas.py
@@ -1,1 +1,9 @@
-# Clase Sistema_Nominas
+class Sistema_Nominas:
+    # parámetro - una lista de clase Empleado
+    def calcular_nominas(self, empleados):
+        print("Calculando nominas")
+        print("==================")
+        for empleado in empleados:
+            print(f"Nomina para : {empleado.nombre} - {empleado.lenguaje_de_programacion}")
+            print(f"- $ : {empleado.calcular_salario()}")
+
