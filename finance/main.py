37df969e69256c726238dbb11acfe84e24b76068
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-09 16:53:05 +0100

    Update main.py

--- finance/main.py
+++ finance/main.py
@@ -5,22 +5,20 @@ class Empleado:
         self.exp        = exp
         self.pais       = pais
         self.ciudad     = ciudad
-        self.__salario  = 2000
+        self._salario  = 2000
         if self.exp >= 2:
-            self.__salario += 200
+            self._salario += 200
         elif self.exp >= 5:
-            self.__salario += 500
+            self._salario += 500
         elif self.exp >= 10:
-            self.__salario += 1000
-        self.__sueldo = self.__salario
+            self._salario += 1000
+        # self.__sueldo = self._salario # A revisar
 
+    def calcular_salario(self):
+        return self._salario
 
-
-    def calcular_salario():
-        pass
-
-    def get_salario():
-        pass
+    def get_salario(self):
+        return self._salario
 
     
 class Programador(Empleado):
@@ -38,8 +36,10 @@ class Programador(Empleado):
             self.__salario_bonus += 1300
         self.__sueldo = self.__salario_bonus
     
+    def calcular_salario(self):
+        return self.__salario_bonus + self._salario
+
     def __info(self):
-        pass
         print(f"Nombre: {self.nombre}; Apellido: {self.apellido}; Exp: {self.exp}; Pais: {self.pais}; Ciudad: {self.ciudad}; Lenguaje de Progrmación: {self.lenguaje_de_programacion}")
 
     def get_info(self):
@@ -49,16 +49,15 @@ class Programador(Empleado):
         print(f"Sueldo: {self.__sueldo}")
 
 class Sistema_Nominas:
-
+    # def __init__(self, nombre, apellido, exp, pais, ciudad, lenguaje_de_programacion):
+    #     super().__init__(nombre, apellido, exp, pais, ciudad, lenguaje_de_programacion)
     # parámetro - una lista de clase Empleado
     def calcular_nominas(self, empleados):
         print("Calculando nominas")
         print("==================")
         for empleado in empleados:
             print(f"Nomina para : {empleado.nombre} - {empleado.lenguaje_de_programacion}")
-            print(f"- $ : {empleado.calcular_nomina}")
-
-
+            print(f"- $ : {empleado.calcular_salario()}")
 
 
 
@@ -68,24 +67,25 @@ if __name__ == "__main__":
     
     # Ejecutar el método para calcular los salarios
     nominas = Sistema_Nominas()
-    nominas.calcular_nominas(empleados)
-    print()
-
-    
+        
     # rellenar la lista de empleados con datos de diferentes tipos de empleados
-
-    # Jon, programador de Python
-    # Maria, progrmadora de Java
-    # Leo, programador de HTML
 
     elliot = Programador("Elliot", "Alderson", 5, "EEUU", "NYC", "Python")
     # elliot.get_info()
     naira = Programador("Naira", "Guirado", 0, "Euskadi", "Donostia", "Java")
     steve = Programador("Steve", "Wozniak", 8, "EEUU", "San José", "Assembly")
-    empleados.append(elliot)
+    empleados.append(elliot
+                     )
     empleados.append(naira)
     empleados.append(steve)
+    nominas.calcular_nominas(empleados)
 
+    print()
     for empleado in empleados:
         empleado.get_salario()
     elliot.get_salario()
+    print(elliot.calcular_salario())
+    print(naira.calcular_salario())
+    print(steve.calcular_salario())
+
+    # empleados.calcular_nominas()
