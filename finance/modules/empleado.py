0e563bc0094407aeb9627571cdecd50c1f64b590
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-12 21:04:38 +0100

    Update empleado.py

--- finance/modules/empleado.py
+++ finance/modules/empleado.py
@@ -1,21 +1,28 @@
+# Clase Empleado
 class Empleado:
-    def __init__(self, nombre, apellido, exp, pais, ciudad):
-        self.nombre     = nombre
-        self.apellido   = apellido
-        self.exp        = exp
-        self.pais       = pais
-        self.ciudad     = ciudad
-        self._salario  = 2000
-        if self.exp >= 2:
-            self._salario += 200
-        elif self.exp >= 5:
-            self._salario += 500
-        elif self.exp >= 10:
+    def __init__(self, nombre, apellido, exp, pais, ciudad): # Constructor con los distintos atributos de la clase Empleado
+        # Iniciamos los atributos de la clase Empleado:
+        self.nombre     = nombre    # Nombre
+        self.apellido   = apellido  # Apellido
+        self.exp        = exp       # Años de experiencia del empleado
+        self.pais       = pais      # País
+        self.ciudad     = ciudad    # Ciudad
+        self.role       = " "      # Role en la start-up
+        self._salario   = 2000      # Sueldo base de un empleado mediante la variable protegida _salario (accesible por distintas clases)
+        
+        # Creamos un sistema para identificar el tiempo de experiencia de trabajo de cada empleado
+        # para que así cobre de acuerdo a lo que se le debe.
+        if self.exp < 2: # ——————————————————— Empleado JUNIOR cobrara + 100€
+            self._salario += 100
+        elif self.exp >= 2 and self.exp < 6: # Empleado SEMI-SENIOR: cobrará + 600€
+            self._salario += 600 
+        elif self.exp >= 6: #————————————————— Empleado SENIOR cobrará + 1000€
             self._salario += 1000
-        # self.__sueldo = self._salario # A revisar
 
-    def calcular_salario(self):
+    # Método para calcular el salario
+    def calcular_salario(self): 
         return self._salario
 
-    def get_salario(self):
+    # método para devolver el salario
+    def get_salario(self): 
         return self._salario
