19c0f53c6c966b2879c2be68717862892eda6539
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-12 21:05:46 +0100

    Update programador.py

--- finance/modules/programador.py
+++ finance/modules/programador.py
@@ -1,29 +1,38 @@
-from empleado import *
+# Importamos todo de la clase Empleado
+from modules.empleado import *
 
-class Programador(Empleado):
-    def __init__(self, nombre, apellido, exp, pais, ciudad, lenguaje_de_programacion):
-        super().__init__(nombre, apellido, exp, pais, ciudad)
-        self.lenguaje_de_programacion = lenguaje_de_programacion
-        self.__salario_bonus = 0
-        if self.lenguaje_de_programacion == "HTML":
-            self.__salario_bonus += 100
-        elif self.lenguaje_de_programacion == "Python":
-            self.__salario_bonus += 400
-        elif self.lenguaje_de_programacion == "Java":
-            self.__salario_bonus += 850
-        elif self.lenguaje_de_programacion == "Assembbly":
-            self.__salario_bonus += 1300
-        self.__sueldo = self.__salario_bonus
-    
+# Clase Programador  
+class Programador(Empleado): # la clase Programador hereda la clase Empleado (lo introducimos como parametro), con sus respectivos atributos y métodos.
+    def __init__(self, nombre, apellido, exp, pais, ciudad, lenguaje_de_programacion): # Constructor con los distintos atributos de la clase Programador
+        super().__init__(nombre, apellido, exp, pais, ciudad) # Utillizamos super().__init__(bla, bla, bla) para heredar atributos y metedos de la clse Empleado.
+        self.role = "Programador/a"                                # Role del empleado: programador
+        self.lenguaje_de_programacion = lenguaje_de_programacion   # Lenguje de programación que domina el programador
+        self._salario_bonus = 0                                    # Establecemos el salario bonus en 0
+
+        # Creamos un sistema para identificar el tipo de lenguaje de cada empleado
+        # para que así cobre de acuerdo a lo que se le debe.
+        if self.lenguaje_de_programacion == "HTML":         # Saber HTML cobrará + 100€    
+            self.role = self.role + " HTML"
+            self._salario_bonus += 100
+        elif self.lenguaje_de_programacion == "Python":     # Saber Python cobrará + 400€
+            self.role = self.role + " Python"
+            self._salario_bonus += 400
+        elif self.lenguaje_de_programacion == "Java":       # Saber Java cobrará + 850€
+            self.role = self.role + " Java"
+            self._salario_bonus += 850
+        elif self.lenguaje_de_programacion == "Assembly":   # Saber Assembly cobrará + 1300€
+            self.role = self.role + " Assembly"
+            self._salario_bonus += 1300
+
+    # Método para calcular el salario    
     def calcular_salario(self):
-        return self.__salario_bonus + self._salario
+        return self._salario_bonus + self._salario
 
+    # Método para privado que imprime la informción del empleado programador    
     def __info(self):
-        pass
-        print(f"Nombre: {self.nombre}; Apellido: {self.apellido}; Exp: {self.exp}; Pais: {self.pais}; Ciudad: {self.ciudad}; Lenguaje de Progrmación: {self.lenguaje_de_programacion}")
+        print(f"\n------------------------------------\nInforamación de la persona empleada:\n------------------------------------")
+        print(f"Nombre: {self.nombre}\nApellido: {self.apellido}\nExp: {self.exp} años\nPais: {self.pais}\nCiudad: {self.ciudad}\nPuesto: {self.role}\nSalario: {self._salario} $\n")
 
+    # Métdo para devolver la información del empleado programador
     def get_info(self):
         return self.__info()
-
-    def get_salario(self):
-        print(f"Sueldo: {self.__sueldo}")
