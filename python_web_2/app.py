be349b0120700cc481706f1aa9ed59e8b1fd94fd
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-14 00:38:25 +0100

    Update app.py

--- python_web_2/app.py
+++ python_web_2/app.py
@@ -6,16 +6,26 @@ app = Flask(__name__)
 class Producto:
     def __init__(self, marca, modelo, precio, electrodos):
         self.marca      = marca
-        self.modele     = modelo
+        self.modelo     = modelo
         self.precio     = precio
         self.electrodos = electrodos
 
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    napse = Producto("NIT", "Napse", 250, 4)
+    egx   = Producto("Upside Down Labs", "EXG Pill", 45, 2)
+
+    nit = "Neural Interface Technologies"
+
+    productos = [napse, egx]
+
+    return render_template("index.html", nit = nit, pd1 = napse, pd2 = egx, productos = productos)
 
 @app.route("/proposito")
-def inicio(): # Es posible que no pueda repetir el nombre
+def proposito(): # Es posible que no pueda repetir el nombre
     return render_template("proposito.html")
 
 
+if __name__ == "__main__":
+
+    app.run()
