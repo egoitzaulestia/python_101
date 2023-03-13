afa44ed93726375c9b2723f764dd10b9def62b03
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-13 17:45:10 +0100

    Create PyhtonWEB

---
+++ Python_Web/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+class Empleado:
+    def __init__(self, nombre, apellido):
+        self.nombre   = nombre
+        self.apellido = apellido
+
+
+@app.route("/")
+def hola():
+    jon = Empleado("Jon", "Smith")
+    # nombre = "Jon"
+    # apellido = "Smith"
+    # return render_template("index.html", name = nombre, lastname = apellido)
+    return render_template("index.html", empleado = jon)
+
+
+@app.route("/adios")
+def adios():
+    # sHTML = "<h1>ADIOOOOOOOS</h1>"
+    # sHTML = sHTML + "<h2>ADIOOOOOOOS</h2>"
+    return render_template("adios.html")
+
+@app.route("/middle")
+def middle():
+    return render_template("middle.html")
+
+if __name__ == "__main__":
+
+
+    app.run()
