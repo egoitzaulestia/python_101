c3004b91f992ccacdaed0df63746dfcec8558591
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-03-15 14:12:36 +0100

    Update app.py

--- Python_Web/app.py
+++ Python_Web/app.py
@@ -1,57 +1,41 @@
 from flask import Flask, render_template, redirect, url_for
 
-
 app = Flask(__name__)
 
-listaPersonas = ["Maria", "Juan", "Jon", "Isabel", "Diego", "Ego"]
-
-listaObjs = []
-
-class Empleado:
-    def __init__(self, id, nombre, apellido, edad):
-        self.id       = id
-        self.nombre   = nombre
-        self.apellido = apellido
-        self.edad     = edad
-
-maria = Empleado(1, "Maria", "Rubio", 34)
-juan = Empleado(2, "Juan", "Bravo", 58)
-jon = Empleado(3, "Jon", "Smith", 25)
-isabel = Empleado(4, "Isabel", "Padilla", 63)
-listaObjs.append(maria)
-listaObjs.append(juan)
-listaObjs.append(jon)
-listaObjs.append(isabel)
-
+coches = []
+coches2 = []
 
 class Coche:
-    def __init__(self, id, image, marca, modelo, features, precioContado, precioFinanciado):
+    def __init__(self, id, marca, modelo, features, precioContado, precioFinanciado, image):
         self.id               = id
-        self.image            = image
         self.marca            = marca
         self.modelo           = modelo
         self.features         = features
         self.precioContado    = precioContado
         self.precioFinanciado = precioFinanciado
+        self.image            = image
 
-coches = []
 
-coche1 = Coche(1, "PEUGEOT", "308", "308 1.2 pUREtECH S&S Alture Pack 102kW", 22900, 19913)
-coche2 = Coche(2, "AUDI", "A5", "A5 Sportback 2.0 TFSI 140kW", 25900, 23900)
-coche3 = Coche(3, "RENAULT", "Mégane", "Mégane 1.5dCi Energy Intens 66kW", 12300, "")
+coche1 = Coche(1,"PEUGEOT", "308", "308 1.2 pUREtECH S&S Alture Pack 102kW", 22900, 19913, "coche1.jpg")
+coche2 = Coche(2, "AUDI", "A5", "A5 Sportback 2.0 TFSI 140kW", 25900, 23900, "coche2.jpg")
+coche3 = Coche(3, "RENAULT", "Mégane", "Mégane 1.5dCi Energy Intens 66kW", 12300, "", "coche3.jpg")
 
 coches.append(coche1)
 coches.append(coche2)
 coches.append(coche3)
 
+coche4 = Coche(4,"PEUGEOT", "308", "308 1.2 pUREtECH S&S Alture Pack 102kW", 22900, 19913, "coche1b.jpg")
+coche5 = Coche(5, "AUDI", "A5", "A5 Sportback 2.0 TFSI 140kW", 25900, 23900, "coche2b.jpg")
+coche6 = Coche(6, "RENAULT", "Mégane", "Mégane 1.5dCi Energy Intens 66kW", 12300, "", "coche3b.jpg")
+
+coches2.append(coche4)
+coches2.append(coche5)
+coches2.append(coche6)
+
+
 @app.route("/")
 def hola():
-    jon = Empleado(1, "Jon", "Smith", 43)
-    # nombre = "Jon"
-    # apellido = "Smith"
-    # return render_template("index.html", name = nombre, lastname = apellido)
-    return render_template("index.html", empleado = jon)
-
+    return render_template("index.html")
 
 @app.route("/aboutus")
 def aboutus():
@@ -59,25 +43,27 @@ def aboutus():
     # print("fasdf")
     return app.send_static_file("aboutus.html")
 
-
-@app.route("/personas")
-def personas():
-    return render_template("personas.html", personas = listaPersonas, empleados = listaObjs)
+# @app.route("/personas")
+# def personas():
+#     return render_template("personas.html", personas = listaPersonas, empleados = listaObjs)
 
 
 @app.route("/coches")
 def cars():
-    return render_template("coches.html", coches = coches)
+    return render_template("coches.html", coches = coches, coches2 = coches2)
     
-@app.route("/dinamica/<int:id>")
+@app.route("/coches/<int:id>")
 def dicamica(id):
-    if id == 10:
-        return "Has seleccionado dinámica 10!!!"
+    if id == 1:
+        return render_template("coche1.html")
+    elif id == 2:
+        return render_template("coche2.html")
+    elif id == 3:
+        return render_template("coche3.html")
     else:
         # return redirect("/index")
         return redirect(url_for("hola")) # El nombre debe ser la misma que la definición de la ruta a la que nos dirigimas
     
-
 
 if __name__ == "__main__":
 
