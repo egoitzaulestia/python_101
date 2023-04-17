32163a4dd6a7af15cbe7de79e35fd524a938d1af
Egoitz Aulestia <49457499+egoitzaulestia@users.noreply.github.com>
2023-04-17 17:30:19 +0200

    Add files via upload

---
+++ x_funcion.py
@@ -0,0 +1,13 @@
+# Crear función de sumar
+
+def sumar(a, b, c):
+    return a + b + c
+
+
+if __name__ == "__main__":
+    a = 3
+    b = 5
+    c = 5
+
+    print(sumar(a, b, c))
+    print(sumar(2, 5, 5))
